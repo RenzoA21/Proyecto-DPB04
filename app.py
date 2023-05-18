0ea6c23067b0bd666923ac954a16183488b14e4d
Martin3007 <91271457+Martin3007@users.noreply.github.com>
2023-05-18 00:49:19 -0500

    Imports and config

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from server import *
+
+import os
+
+os.environ['PGPASSWORD'] = '12345'
+
+from flask import (Flask, request, render_template)
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/Pharmacy'
+db = SQLAlchemy(app)
 
