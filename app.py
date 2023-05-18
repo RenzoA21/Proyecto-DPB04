ffc8b804844a9fd52f56c864f2926046494879d9
Martin3007 <91271457+Martin3007@users.noreply.github.com>
2023-05-18 00:52:06 -0500

    context and allowed extensions

--- app.py
+++ app.py
@@ -11,3 +11,16 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/Pharmacy'
 db = SQLAlchemy(app)
 
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5006)
+else:
+    print('Importing {}'.format(__name__))
+    
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOW_EXTENSIONS
+
+
+
